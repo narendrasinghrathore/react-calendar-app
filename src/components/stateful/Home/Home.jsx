0ee0880429a8f4ec64d1678be0b47e93d773bb36
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2020-05-10 17:21:17 +0530

    Deploy from github to firebase using travis-ci https://www.youtube.com/watch?v=QLVzozWDYAs

--- src/components/stateful/Home/Home.jsx
+++ src/components/stateful/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Calendar from '../../stateless/Calendar/Calendar';
 export default function Home() {
-    return <div><h1>Home</h1></div>;
+    return <div><h1>Home</h1>
+        <Calendar /></div>;
 }
---
+++ src/components/stateless/Calendar/Calendar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function Calendar() {
+    return <>
+        <h3>
+            Calendar</h3></>;
+}
