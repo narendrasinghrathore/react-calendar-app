67504124a8e6365ab83f6cb7526ad1340ef61c50
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-06-30 10:49:43 +0530

    scss added, styling of component

--- src/components/stateless/Month/Month.tsx
+++ src/components/stateless/Month/Month.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
+import './Month.scss';
 import { useSelector, useDispatch } from "react-redux";
 import { getTotalNumberOfDays, getMonthDays } from "../../../store";
+import { cardsContainer } from "../../../styles/month";
 
 export default function Month() {
   const [month, year] = [new Date().getMonth(), new Date().getFullYear()];
@@ -13,10 +15,10 @@ export default function Month() {
   }, [dispatch, month, year]);
 
 
-  return <>
+  return <div style={cardsContainer}>
     {Array.from(Array(totalDays), (_, i) => {
-      return <div key={i}>
+      return <div className="cards" key={i}>
         {i + 1}
       </div>
-    })}</>;
+    })}</div>;
 }
---
+++ src/styles/month.ts
@@ -0,0 +1,25 @@
+import { CSSProperties } from "react";
+
+/**
+ * Constructs a type with all properties of T set to optional.
+ * This utility will return a type that represents
+ * all subsets of a given type
+ */
+type MyStyles = Partial<CSSProperties>;
+
+const cards: CSSProperties = {
+  boxShadow: "2px 2px 1px 0px #999",
+  margin: 3,
+  padding: 10,
+  borderRadius: 3,
+  width: 50,
+  height: 50,
+};
+
+const cardsContainer: CSSProperties = {
+  display: "flex",
+  flexWrap: "wrap",
+  margin: "10 auto",
+};
+
+export { cards, cardsContainer };
