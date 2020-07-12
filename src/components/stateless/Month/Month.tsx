d2047c98261166471d5b27f0fc09e37a42ed46fb
nrathoreplulmtree <67887155+nrathoreplulmtree@users.noreply.github.com>
2020-07-12 11:37:54 +0530

    designing date picker

--- src/components/stateless/Month/Month.tsx
+++ src/components/stateless/Month/Month.tsx
@@ -126,8 +126,11 @@ export default function Month() {
 
 
   return <div>
+    {/* <DatePicker selectedDate={todayDate} onChange={(e) => updateDate(e)} /> */}
+
     <div className="date-picker">
       <input type="date" onChange={(e) => updateDate(e)} value={todayDate} />
+
       <Chevron />
     </div>
     <div className="weekNames">
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import * as firebaseFunctions from "firebase/firebase-functions";
 
 import { Provider } from "react-redux";
 import configureStore from "./store";
 
 const store = configureStore({});
-const value = firebaseFunctions.config().api;
-console.log(value);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
